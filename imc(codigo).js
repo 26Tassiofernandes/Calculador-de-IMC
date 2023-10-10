c5d522e8d86d69c5b22f5cebb1fc463c9bfa54b7
Tássio <86972667+26Tassiofernandes@users.noreply.github.com>
2023-10-10 18:35:48 -0300

    Update imc(codigo).js

--- imc(codigo).js
+++ imc(codigo).js
@@ -20,16 +20,16 @@ function calc() {
     } else if (imc < 18.5) {
         msg.innerHTML = `Seu imc é de ${imc.toFixed(2)} e você está abaixo do peso!`
 
-    } else if (imc >= 18.6 && imc <= 24.9) {
+    } else if (imc >= 18.6 && imc <= 24.99) {
         msg.innerHTML = `Seu imc é de ${imc.toFixed(2)} e você está no peso ideal, parabéns!`
 
-    } else if (imc >= 25 && imc <= 29.9) {
+    } else if (imc >= 25 && imc <= 29.99) {
         msg.innerHTML = `Seu imc é de ${imc.toFixed(2)} e você está levemente acima do peso!`
 
-    } else if (imc >= 30 && imc <= 34.9) {
+    } else if (imc >= 30 && imc <= 34.99) {
         msg.innerHTML = `Seu imc é de ${imc.toFixed(2)} e você está em obesidade grau I!`
 
-    } else if (imc >= 35 && imc <= 39.9) {
+    } else if (imc >= 35 && imc <= 39.99) {
         msg.innerHTML = `Seu imc é de ${imc.toFixed(2)} e você está em obesidade severa!`
 
     } else {
