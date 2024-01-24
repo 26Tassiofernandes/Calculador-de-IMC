3c2c92935c9cd7510a8d4189c30c318f3740f26d
26TassioFernandes <tassiofernandes881@gmail.com>
2024-01-23 22:25:31 -0300

    update

--- imc(codigo).js
+++ imc(codigo).js
@@ -20,19 +20,19 @@ function calc() {
     } else if (imc < 18.5) {
         msg.innerHTML = `Seu imc é de ${imc.toFixed(2)} e você está abaixo do peso!`
 
-    } else if (imc >= 18.6 && imc <= 24.99) {
+    } else if (imc >= 18.5 && imc < 24.9) {
         msg.innerHTML = `Seu imc é de ${imc.toFixed(2)} e você está no peso ideal, parabéns!`
 
-    } else if (imc >= 25 && imc <= 29.99) {
+    } else if (imc >= 25 && imc < 29.9) {
         msg.innerHTML = `Seu imc é de ${imc.toFixed(2)} e você está levemente acima do peso!`
 
-    } else if (imc >= 30 && imc <= 34.99) {
+    } else if (imc >= 30 && imc < 34.9) {
         msg.innerHTML = `Seu imc é de ${imc.toFixed(2)} e você está em obesidade grau I!`
 
-    } else if (imc >= 35 && imc <= 39.99) {
+    } else if (imc >= 35 && imc < 39.9) {
         msg.innerHTML = `Seu imc é de ${imc.toFixed(2)} e você está em obesidade severa!`
 
-    } else {
+    } else if (imc > 40) {
         msg.innerHTML = `Seu imc é de ${imc.toFixed(2)} e você está em obesidade mórbida!`
         
     }
